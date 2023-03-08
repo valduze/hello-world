905e4f084d0dcce2b7d3fc52952ca49c9cf40581
Jongilanga Guma <jongi@ip-192-168-0-199.us-west-1.compute.internal>
2023-03-08 10:28:02 +0200

    adding my name, just to make sure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Jongi's Hello World vue app"/>
 </template>
 
 <script>
